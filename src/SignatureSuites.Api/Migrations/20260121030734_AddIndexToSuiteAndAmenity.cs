7db6560bb48d0c8306541eb41a31058bb6d75680
garibaycarlos <garibaycarlos@live.com>
2026-01-20 21:40:45 -0600

    Add migration to create index on Name fields for Suite and Amenity.

---
+++ src/SignatureSuites.Api/Migrations/20260121030734_AddIndexToSuiteAndAmenity.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SignatureSuites.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexToSuiteAndAmenity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Suite_Name",
+                table: "Suite",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Amenity_Name",
+                table: "Amenity",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Suite_Name",
+                table: "Suite");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Amenity_Name",
+                table: "Amenity");
+        }
+    }
+}
